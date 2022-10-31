14548d4549402adb6404e6eab1659b25b8f5e03e
Keelyy <ikeelyy@gmail.com>
2022-10-31 22:39:09 +0700

    js

--- main.js
+++ main.js
@@ -1,56 +1,30 @@
-// Bài 3
-document.getElementById("btnBai3").onclick = function () {
-    // input
-    var nhapSo = document.getElementById("nhapSo").value * 1;
+// Bài 5
+document.getElementById('btnBai5').onclick = function () {
+    // input 
+    var number = document.getElementById("number").value * 1;
 
-    //output
-    var giaiThua = 1;
-    // B1: Xác định gía trị khởi tạo nó
-    var giaTri = 1;
-    //B2: Thiết lập đk lặp
-    while (giaTri <= nhapSo) {
-        //Bước 3: Xử lý
-        giaiThua *= giaTri;
-        //Bước 4: Thay đổi đk lặp
-        giaTri++;
-    }
-
-    //Bước 5: In kết quả
-    document.getElementById("ketQuaBai3").innerHTML = giaiThua;
-}
-
-// Bài 1
-document.getElementById('btnBai1').onclick = function () {
     // output
-    var tong = 0;
-    var soHang = 1;
-    while (tong <= 10000) {
-        soHang++;
-        tong += soHang;
+    var ketQua = "";
+
+    for (iso = 2; iso <= number; iso++) {
+        var checkSNT = kiemTraSoNguyenTo(iso);
+        //Nếu true là số nguyên tố
+        if (checkSNT) {
+            ketQua += iso + " ";
+        }
     }
-    document.getElementById('ketQuaBai1').innerHTML = soHang;
+    document.getElementById("ketQuaBai5").innerHTML = ketQua;
 }
 
-// Bài 2
-document.getElementById('btnBai2').onclick = function () {
-    var number1 = document.getElementById('number1').value * 1;
-    var number2 = document.getElementById('number2').value * 1;
-
-        // Output
-    var tongMu = 0;
-    //Xác định giá trị khởi tạo nó
-    var soMu = 1;
-    var luyThua = 0;
-    
-    while(soMu <= number2){
-        luyThua = Number(Math.pow(number1,soMu));
-        soMu ++;
-        tongMu += luyThua;
+function kiemTraSoNguyenTo(number) {
+    //output: true or false
+    var checkSNT = true;
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            checkSNT = false;
+            break;
+        }
     }
-    document.getElementById('ketQuaBai2').innerHTML = tongMu;
+    return checkSNT;
 }
 
-// Bài 4
-document.getElementById('btnBai4').onclick = function(){
-    
-}
